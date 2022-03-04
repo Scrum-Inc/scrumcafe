a982caa00fa861a4077f0c5181d853356f28bb3f
Natalia Galán <hi@nataliagalan.me>
2022-03-04 09:32:07 -0600

    wip: display options

--- src/components/Menu/Item/index.js
+++ src/components/Menu/Item/index.js
@@ -1,4 +1,5 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Styled from "./styles";
 
@@ -8,6 +9,9 @@ const Item = ({ item, onClick, disabled }) => {
 			<Col>
 				<Styled.Name>{item.name}</Styled.Name>
 				<Styled.Description>{item.description}</Styled.Description>
+				{item.options.map((itemOption) => (
+					<Form.Check.Input id={itemOption} label={itemOption.label} />
+				))}
 			</Col>
 			<Col sm="auto">
 				<Button onClick={() => onClick(item)} disabled={disabled}>
