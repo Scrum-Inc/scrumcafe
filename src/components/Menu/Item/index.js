7542ef3b79334dc6575cff33497256b2ad04551c
david_bille <David.Bille@precisionvh.com>
2022-03-04 09:57:40 -0600

    final changes to checkboxes

--- src/components/Menu/Item/index.js
+++ src/components/Menu/Item/index.js
@@ -1,16 +1,27 @@
 import Col from "react-bootstrap/Col";
+import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Styled from "./styles";
 
-const Item = ({ item, onClick, disabled }) => {
+const Item = ({ item, onClick, disabled, toggleCheckboxOption }) => {
+	
+
 	return (
 		<Styled.MenuItem>
 			<Col>
 				<Styled.Name>{item.name}</Styled.Name>
 				<Styled.Description>{item.description}</Styled.Description>
-				{item.options.map((itemOption) => (
-					<Form.Check.Input id={itemOption} label={itemOption.label} />
+				{item.options.map((itemOption, index) => (
+					
+					<Styled.Description key={index}>
+						<Form.Check 
+							id={itemOption} 
+							label={itemOption.label} 
+							checked={itemOption.selected} 
+							onChange={(e) => toggleCheckboxOption(e)}
+						/>
+					</Styled.Description>
 				))}
 			</Col>
 			<Col sm="auto">
